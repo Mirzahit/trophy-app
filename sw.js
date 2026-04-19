18bbcec8df679da98aef907f151da1adc6745a04
Mirzahit <mirzahit@local>
2026-04-19 09:14:59 +0500

    fix: отключение кэша Vercel для index.html/sw.js, автоперезагрузка при новой версии SW, ?demo=1 теперь подставляет в текущий профиль

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'trophy-v7';
+var CACHE = 'trophy-v8';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', function(e){
@@ -9,10 +9,16 @@ self.addEventListener('message', function(e){
   if(e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 self.addEventListener('activate', function(e){
-  e.waitUntil(caches.keys().then(function(keys){
-    return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
-  }));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
+    }).then(function(){return self.clients.claim()}).then(function(){
+      // Просим все открытые вкладки перезагрузиться чтобы подхватить свежий код
+      return self.clients.matchAll({type:'window'}).then(function(cs){
+        cs.forEach(function(c){c.postMessage({type:'RELOAD'})});
+      });
+    })
+  );
 });
 self.addEventListener('fetch', function(e){
   var url = e.request.url;
